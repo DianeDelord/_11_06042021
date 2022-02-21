cf66960645c1b9145d407148a0ce0346a51acd94
Diane DELORD <82445395+DianeDelord@users.noreply.github.com>
2022-02-21 17:56:58 +0100

    photos + titre card ok sans mise en forme

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import FetchData from '../utils/FetchData';
+import { Link } from 'react-router-dom';
+
 
 const Card = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        FetchData.get('datas-kasa.json')
+          .then(logements => {
+            setData(logements);
+            console.log(logements)
+          })
+      }, []);
+
     return (
         <div className='locCard'>
             
+            {data && (
+          <section className='rental_card'>
+            {data.map((e) => (
+                <Link key={e.id} to={`/rental/${e.id}`} className='card_block'>
+                    <h3 className='card_title'> {e.title} </h3>
+                    <img src={e.cover} />
+                </Link>            
+            ))}
+          </section>
+        )}
+
         </div>
     );
 };
--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,35 +1,10 @@
 import React from 'react';
 import Card from './Card';
-import { useState, useEffect } from 'react';
-import FetchData from '../utils/FetchData';
-import { Link } from 'react-router-dom';
-
 
 const HomeContainer = () => {
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-        FetchData.get('datas-kasa.json')
-          .then(logements => {
-            setData(logements);
-            console.log(logements)
-          })
-      }, []);
 
     return (
         <div className='homecontainer'>
-
-{data && (
-          <section className='home__grid'>
-            {data.map((e) => (
-              <Link key={e.id} to={`/rental/${e.id}`}>
-                <Card title={e.title} imgCover={e.cover} />
-              </Link>
-            ))}
-          </section>
-        )}
-
-
             <Card />
         </div>
     );
