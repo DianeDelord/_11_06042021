e7af06b4b1388bcbfbc31161ebce88fd6449e54a
Diane DELORD <82445395+DianeDelord@users.noreply.github.com>
2022-02-21 15:28:27 +0100

    cartes logements générées (sans photos)

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import Card from './Card';
 import { useState, useEffect } from 'react';
-import FetchService from '../utils/FetchService';
+import FetchData from '../utils/FetchData';
+import { Link } from 'react-router-dom';
 
 
 const HomeContainer = () => {
     const [data, setData] = useState([]);
 
-console.log(data)
     useEffect(() => {
-        FetchService.get('data-kasa.json')
+        FetchData.get('datas-kasa.json')
           .then(logements => {
             setData(logements);
+            console.log(logements)
           })
       }, []);
 
     return (
         <div className='homecontainer'>
+
+{data && (
+          <section className='home__grid'>
+            {data.map((e) => (
+              <Link key={e.id} to={`/rental/${e.id}`}>
+                <Card title={e.title} imgCover={e.cover} />
+              </Link>
+            ))}
+          </section>
+        )}
+
+
             <Card />
         </div>
     );
