c3c4ae3f45372ea1102ca63680fbdadc52400889
Diane DELORD <82445395+DianeDelord@users.noreply.github.com>
2022-02-24 09:06:13 +0100

    bandeau photo page "a propos"

--- src/components/Bandeau.jsx
+++ src/components/Bandeau.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import backgroundPicture from '../visuels/kalen-emsley-Bkci_8qcdvQ-unsplash 2.jpg'
 
 const Bandeau = () => {
     return (
         <div>
-            
+              <div className='navPicture'>
+                <img src={backgroundPicture} alt="paysage de cimes de montagnes" className='background-picture' />
+                <h1 className='catchphrase'></h1>
+            </div>
         </div>
     );
 };
